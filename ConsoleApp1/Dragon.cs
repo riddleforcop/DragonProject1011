f064602ea3bf57aa40c888598b6d3435b2fe6606
Артём Топалян <TAVSch.22@uni-dubna.ru>
2023-07-04 01:20:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Dragon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Dragon
+    {
+        public int HealthPoints { get; set; }
+        public int Gun { get; set; }
+        public int Food { get; set; }
+        public int Reputation { get; set; }
+        public int Hunger { get; set; }
+        public Dragon() 
+        {
+            HealthPoints = 0;
+            Gun = 0;
+            Food = 0;
+            Reputation = 0;
+            Hunger = 0;
+        }
+        public Dragon(int healthPoints, int gun, int food)
+        {
+            HealthPoints = healthPoints;
+            Gun = gun;
+            Food = food;
+            Reputation = 0;
+            Hunger = 0;
+        }
+    }
+    public class MyLittleDragon : Dragon
+    {
+        public MyLittleDragon() : base(70, 15, 25)
+        {}
+    }
+    public class OrdinaryDragon : Dragon
+    {
+        public OrdinaryDragon() : base(50, 10, 40)
+        {}
+    }
+    public class VillageNightmare : Dragon
+    {
+        public VillageNightmare() : base(150, 25, 30)
+        {}
+    }
+}
